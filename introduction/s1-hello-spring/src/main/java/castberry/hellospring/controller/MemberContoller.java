a9e6840f84acfca67625ee234697ed9bca456ae4
castberry10 <soc09072@gmail.com>
2024-04-01 02:23:35 +0900

    웹 - 등록 조회 추가

--- introduction/s1-hello-spring/src/main/java/castberry/hellospring/controller/MemberContoller.java
+++ introduction/s1-hello-spring/src/main/java/castberry/hellospring/controller/MemberContoller.java
@@ -4,6 +4,11 @@ import castberry.hellospring.domain.Member;
 import castberry.hellospring.sevice.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberContoller {
@@ -12,5 +17,28 @@ public class MemberContoller {
     @Autowired
     public MemberContoller(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
+
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
