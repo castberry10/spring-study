a5a2a0398ec24ab44e5d14c2c4bd1a8614fce094
castberry10 <soc09072@gmail.com>
2024-06-23 18:25:17 +0900

    jdbc 사용 및 다형성

--- introduction/s1-hello-spring/src/main/java/castberry/hellospring/SpringConfig.java
+++ introduction/s1-hello-spring/src/main/java/castberry/hellospring/SpringConfig.java
@@ -1,15 +1,26 @@
 package castberry.hellospring;
 
+import castberry.hellospring.repository.JdbcMemberRepository;
 import castberry.hellospring.repository.MemberRepository;
 import castberry.hellospring.repository.MemoryMemberRepository;
 import castberry.hellospring.sevice.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 // 만약 회원서비스와 회원레포지토리의 @Service, @Repository, @Autowired 애노테이션이 없다면
 // 이처럼 해야함
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService(){
@@ -18,6 +29,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository(){
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
