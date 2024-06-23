1638527430ba0c01f143007368129523f290fc99
castberry10 <soc09072@gmail.com>
2024-06-23 19:02:33 +0900

    스프링 통합 테스트

--- introduction/s1-hello-spring/src/test/java/castberry/hellospring/service/MemverSerciveIntegrationTest.java
+++ introduction/s1-hello-spring/src/test/java/castberry/hellospring/service/MemverSerciveIntegrationTest.java
@@ -1,4 +1,68 @@
 package castberry.hellospring.service;
 
-public class MemverSerciveIntegrationTest {
+import castberry.hellospring.domain.Member;
+import castberry.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+// @SpringBootTest
+// - 스프링 컨테이너와 테스트를 함께 실행
+// @트랜젝션
+// - 테스트 케이스에 있으면 테스트 시작전에 트랜젝션을 시작하고 테스트 완료 후에 롤백한다
+// - 연속적인 테스트가 가능하다 <- Test에서만 이럼 서비스에서는 커밋됨
+// 롤백을 원치않으면 테스트케이스에 @Commit을 작성해도 된다
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+    @Autowired MemberService memberService;
+    @Autowired MemoryMemberRepository memberRepository;
+
+
+    // 디비를 지우고 해야함
+    // -> 그래서 보통 테스트 디비를 따로 만든다
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("hello");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+
+    }
+
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+//        try{
+//            memberService.join(member2);
+//            fail();
+//        }catch (IllegalStateException e){
+//            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+//        }
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+        //then
+
+    }
+
 }
